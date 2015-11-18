db1234b978ee4940def9682486abbb898615297e
chengli <chengli@rd.com>
2015-11-18 11:01:31 +0800

    update

--- bean_init/src/main/java/com/chengli/bean/init/OuterBean.java
+++ bean_init/src/main/java/com/chengli/bean/init/OuterBean.java
@@ -14,37 +14,42 @@ import org.springframework.stereotype.Service;
  * ③ 如果bean实现了BeanNameAware则调用setBeanName方法，将Bean的ID传递给Bean
  * ④ 如果bean实现了BeanFactoryAware则调用setBeanFactory方法
  * ⑤ 如果实现了ApplicationContextAware则调用setApplicationContext
+ * ⑥ 如果实现了BeanPostProcessor 则调用postProcessBeforeInitialization
+ * ⑦ 如果实现了InitializingBean 则调用afterPropertiesSet，如果该方法用init-method实现了初始化方法，afterPropertiesSet也会被调用
+ * ⑧ 如果实现了BeanPostProcessor 则调用postProcessAfterInitialization
+ * ⑨ 如果实现了DisposableBean 则调用destroy
+ *
  * Created by chengli on 2015/11/17.
  */
 @Service
 public class OuterBean implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, BeanPostProcessor, InitializingBean, DisposableBean {
 
     public OuterBean() {
-        System.out.println("OuterBean contrustor");
+        System.out.println("① OuterBean contrustor");
     }
 
     /**
      * @param name
      */
     public void setBeanName(String name) {
-        System.out.println("set OuterBean name:" + name);
+        System.out.println("③ set OuterBean name:" + name);
     }
 
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        System.out.println("set OuterBean beanFactory:" + beanFactory);
+        System.out.println("④ set OuterBean beanFactory:" + beanFactory);
     }
 
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.println("set OuterBean applicationContext:" + applicationContext);
+        System.out.println("⑤ set OuterBean applicationContext:" + applicationContext);
     }
 
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("postProcessBeforeInitialization");
-        return null;
+        System.out.println("⑥ postProcessBeforeInitialization");
+        return new Object();
     }
 
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("postProcessAfterInitialization");
+        System.out.println("⑧ postProcessAfterInitialization");
         return null;
     }
 
@@ -53,6 +58,6 @@ public class OuterBean implements BeanNameAware, BeanFactoryAware, ApplicationCo
     }
 
     public void afterPropertiesSet() throws Exception {
-        System.out.println("after properties set~!~");
+        System.out.println("⑦ after properties set~!~");
     }
 }
